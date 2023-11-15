bafa62835e87bf82ce728c86832a28d4645f71b5
Mpablom <100524470+Mpablom@users.noreply.github.com>
2023-11-14 23:54:03 -0300

    actualizado

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -4,33 +4,34 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
-      })
+    setup = function(_, opts)
+      -- Asegurarse de que opts.ensure_installed exista y sea una tabla
+      opts.ensure_installed = opts.ensure_installed or {}
+      -- Añadir más elementos a la tabla opts.ensure_installed
+      local astronvim_utils = require "astronvim.utils"
+      opts.ensure_installed = astronvim_utils.list_insert_unique(opts.ensure_installed, "lua_ls")
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
-      })
+    setup = function(_, opts)
+      -- Asegurarse de que opts.ensure_installed exista y sea una tabla
+      opts.ensure_installed = opts.ensure_installed or {}
+      -- Añadir más elementos a la tabla opts.ensure_installed
+      opts.ensure_installed = astronvim_utils.list_insert_unique(opts.ensure_installed, "prettier")
+      opts.ensure_installed = astronvim_utils.list_insert_unique(opts.ensure_installed, "skylua")
     end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
-      })
+    setup = function(_, opts)
+      -- Asegurarse de que opts.ensure_installed exista y sea una tabla
+      opts.ensure_installed = opts.ensure_installed or {}
+      -- Añadir más elementos a la tabla opts.ensure_installed
+      opts.ensure_installed = astronvim_utils.list_insert_unique(opts.ensure_installed, "python")
     end,
   },
 }
